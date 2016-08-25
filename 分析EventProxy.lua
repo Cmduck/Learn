44053b59555a3c28ba1f9f7f0dcffac72f29db7f
sozenvN <261524715@qq.com>
2016-08-25 19:03:14 +0800

    modify 分析EventProxy.lua

--- 分析EventProxy.lua
+++ 分析EventProxy.lua
@@ -1,6 +1,6 @@
 --通过代理注册事件的好处：可以方便的在视图删除时，清理所有通过该代理注册的事件，同时不影响目标对象上注册的其他事件
 --EventProxy.new()第一个参数是要注册事件的对象，第二个参数是绑定的视图。如果指定了第二个参数，那么在视图删除时，会自动清理注册的事件.
-
+--实际上还是通过EventProtocol去操作
 local EventProxy = class("EventProxy")
 
 function EventProxy:ctor(eventDispatcher, view)
